6e94a92059ab4237e29b804a8dca755328bff2a1
Rado236 <55925335+Rado236@users.noreply.github.com>
2023-10-14 17:28:07 +0300

    auth test

--- app/src/main/java/com/example/alarmingmobileapp/Login.java
+++ app/src/main/java/com/example/alarmingmobileapp/Login.java
@@ -3,8 +3,24 @@ package com.example.alarmingmobileapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.ktx.Firebase;
 
 public class Login extends AppCompatActivity {
+
+    FirebaseAuth auth;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser=auth.getCurrentUser();
+        if(currentUser != null){
+            Toast.makeText(Login.this,"User is not null",Toast.LENGTH_SHORT);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
